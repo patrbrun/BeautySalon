01103e5ab87bd075b53a6b2e3cfe1966906f914e
sopham <sopham@taltech.ee>
2019-05-19 10:09:44 +0300

    add database properties

--- src/main/java/com/grouptwo/saloon/SaloonApplication.java
+++ src/main/java/com/grouptwo/saloon/SaloonApplication.java
@@ -2,12 +2,18 @@ package com.grouptwo.saloon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.annotation.WebServlet;
 
 @SpringBootApplication
 public class SaloonApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SaloonApplication.class, args);
 	}
 
 }
+
