66b3d7b48be395a6b77e11f899f822f1982aa712
P <paaat29@gmail.com>
2019-05-26 14:36:27 +0300

    Update in SaloonAplication / fix merge bugs

--- src/main/java/com/grouptwo/saloon/SaloonApplication.java
+++ src/main/java/com/grouptwo/saloon/SaloonApplication.java
@@ -1,8 +1,5 @@
 package com.grouptwo.saloon;
 
-import com.grouptwo.saloon.model.Employee;
-import com.grouptwo.saloon.repository.EmployeeRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +7,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 @EnableSwagger2
@@ -23,14 +21,6 @@ public class SaloonApplication {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("com.grouptwo.saloon")).build();
-    }
-
-    @Bean
-    public CommandLineRunner demo(EmployeeRepository repository) {
-        return (args) -> {
-            // save a couple of customers
-            repository.save(new Employee("Jack", "hairdresser", "090"));
-        };
     }
 }
 
